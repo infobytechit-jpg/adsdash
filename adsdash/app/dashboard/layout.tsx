b11270bf6b94cb9385639ed44684481f556b5258
infobytechit-jpg <info.bytechit@gmail.com>
2026-02-24 22:22:28 +0100

    Update layout.tsx

--- adsdash/app/dashboard/layout.tsx
+++ adsdash/app/dashboard/layout.tsx
@@ -1,4 +1,9 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-  export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
 }
